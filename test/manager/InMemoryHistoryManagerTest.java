5c11024a1f0be7df3e21250d79350c7420ee5036
Sergey Shihovcev <Shihovcev.sv@yandex.ru>
2025-04-25 08:44:12 +0300

    Подготовка к написанию тестов

--- test/manager/InMemoryHistoryManagerTest.java
+++ test/manager/InMemoryHistoryManagerTest.java
@@ -15,6 +15,23 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class InMemoryHistoryManagerTest {
+        private HistoryManager historyManager;
+        private Task taskA;
+        private Task taskB;
+        private Task taskC;
+        private Task taskD;
 
+        @BeforeEach
+        void beforeEachTest() {
+            historyManager = Manager.getDefaultHistory();
+            taskA = new Task("Task A", "Description A");
+            taskB = new Task("Task B", "Description B");
+            taskC = new Task("Task C", "Description C");
+            taskD = new Task("Task D", "Description D");
+            taskA.setId(1);
+            taskB.setId(2);
+            taskC.setId(3);
+            taskD.setId(4);
+        }
 
 }
